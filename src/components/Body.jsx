58822e14f8a7d9ed20a0e35af111b097ca9a575a
Alex <alex.muniz@aquare.la>
2025-05-28 22:08:18 -0300

    feat: class initial development

---
+++ src/components/Body.jsx
@@ -0,0 +1,28 @@
+import "./Body.css";
+function Body(props) {
+  console.log(props.characters);
+
+  return (
+    <div className="Body">
+      <div className="grid">
+      {props.characters.map((character) => (
+        <div className="card">
+          {character.image ? (
+            <img src={character.image} alt={character.name} />
+          ) : (
+            <div className="noImage">Imagem indisponível</div>
+          )
+         }
+          <h2>{character.name}</h2>
+          <p>
+            <strong>Casa:</strong> 
+            {character.house || "Desconhecida"}
+          </p>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
+
+export default Body;
